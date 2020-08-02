cdc47b6bda28dcc3c02dbe60c50e715764093a26
b3z3rk_irl <nikhil4709@gmail.com>
2020-08-02 19:04:51 +0530

    Update ZIPF.py

--- ZIPF.py
+++ ZIPF.py
@@ -3,14 +3,14 @@ import PyPDF2
 import textract
 import warnings
 warnings.filterwarnings("ignore")
-import nltk
+# import nltk
 from nltk.tokenize import word_tokenize
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 Tk().withdraw()
 filename = askopenfilename()
 print(filename)
-nltk.download('punkt')
+# nltk.download('punkt')
 pdfFileObj = open(filename,'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 num_pages = pdfReader.numPages
@@ -21,26 +21,33 @@ while count < num_pages:
     count +=1
     text += pageObj.extractText()
 if text != "":
-   text = text
+    k=1
+    text = text
 else:
-   text = textract.process(fileurl, method='tesseract', language='eng')
+    print("text is empty")
+    k=0
+    # text = textract.process(fileurl, method='tesseract', language='eng')
 tokens = word_tokenize(text)
-tokens.remove(".")
+try:
+    tokens.remove(".")
+except:
+    print()
 punctuations = ['(',')',';',':','[',']',',',' ',"'",'.',"''",'+','-','=','*','/',"!","@","#",'’',"$","%","^","&","(",")","?",'...',"-","`",'‘',"``","--","1","2","3","4","5","6","7","8","9","0"]
 from nltk.stem.porter import PorterStemmer
 porter = PorterStemmer()
-keywords = [porter.stem(word.lower()) for word in tokens if not word in punctuations]
-keyset=set(keywords)
-di={}
-for i in keyset:
-    di[i]=keywords.count(i)
-l = sorted(di.items(), key=lambda kv: (kv[1], kv[0]), reverse = True)
-print(l[0:10])
-rdic={}
-for i in l[0:10]:
-    rdic[i[0]]=i[1]
-plt.bar(list(rdic.keys()), rdic.values(), color='g')
-plt.title(filename)
-plt.show(block=False)
-plt.pause(10)
-plt.close()
+if k:
+    keywords = [porter.stem(word.lower()) for word in tokens if not word in punctuations]
+    keyset=set(keywords)
+    di={}
+    for i in keyset:
+        di[i]=keywords.count(i)
+    l = sorted(di.items(), key=lambda kv: (kv[1], kv[0]), reverse = True)
+    print(l[0:10])
+    rdic={}
+    for i in l[0:10]:
+        rdic[i[0]]=i[1]
+    plt.bar(list(rdic.keys()), rdic.values(), color='g')
+    plt.title(filename)
+    plt.show(block=False)
+    plt.pause(10)
+    plt.close()
